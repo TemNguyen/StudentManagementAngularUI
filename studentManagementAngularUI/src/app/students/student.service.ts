877b8c25039bd00f95b82e3e592dd061e770f367
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2022-02-17 11:02:52 +0700

    Day 1: FetchAPI, Compelete GET Students. Add Sort, Filter in table

---
+++ studentManagementAngularUI/src/app/students/student.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from '../models/api-models/students.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private baseApiUrl = 'https://localhost:44367'
+  constructor(private httpClient: HttpClient) { }
+
+  getStudents(): Observable<Student[]> {
+    return this.httpClient.get<Student[]>(this.baseApiUrl + '/student');
+  }
+}
