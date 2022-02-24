4617aab908050f9ee3443ca4fae9f7ff0b6c633d
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2022-02-24 20:18:02 +0700

    blahblah

--- studentManagementAngularUI/server.js
+++ studentManagementAngularUI/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/studentmanagementangularui'));
+app.use(express.static('./dist/studentManagementAngularUI'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('./src/index.html', {root: 'dist/studentmanagementangularui/'}),
+    res.sendFile('./src/index.html', {root: 'dist/studentManagementAngularUI/'}),
 );
 
 // Start the app by listening on the default Heroku port
