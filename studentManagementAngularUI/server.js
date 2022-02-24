51e3350bf0029e79f0355183f889b4b62240980d
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2022-02-24 18:50:18 +0700

    Day 4: Fix Path 2

--- studentManagementAngularUI/server.js
+++ studentManagementAngularUI/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/dotnetcorestudentmanagement'));
+app.use(express.static('./dist/dotnetcorestudentmanagement/studentManagementAngularUI'));
 
 app.get('/*', (req, res) =>
     res.sendFile('./src/index.html', {root: 'dist/dotnetcorestudentmanagement/'}),
