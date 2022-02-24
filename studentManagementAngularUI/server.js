7f6525b1facd5453d85358fd4cc2b500d1c1038b
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2022-02-24 18:09:52 +0700

    Day 4: Fix Path 2

--- studentManagementAngularUI/server.js
+++ studentManagementAngularUI/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/dotnetcorestudentmanagement/studentmanagementangularui'));
+app.use(express.static('./dist/dotnetcorestudentmanagement'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('./src/index.html', {root: 'dist/dotnetcorestudentmanagement'}),
+    res.sendFile('./src/index.html', {root: 'dist/dotnetcorestudentmanagement/studentmanagementangularui'}),
 );
 
 // Start the app by listening on the default Heroku port
