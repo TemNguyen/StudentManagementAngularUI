77cc9f008ef7ce24794e1453f0aba8cc1df35c07
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2022-02-24 20:12:50 +0700

    test deploy

--- studentManagementAngularUI/server.js
+++ studentManagementAngularUI/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/dotnetcorestudentmanagement/studentManagementAngularUI'));
+app.use(express.static('./dist/studentmanagementangularui'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('./src/index.html', {root: 'dist/dotnetcorestudentmanagement/'}),
+    res.sendFile('./src/index.html', {root: 'dist/studentmanagementangularui/'}),
 );
 
 // Start the app by listening on the default Heroku port
